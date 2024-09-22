70d62c69d3a93edef9e42f04756a0a00f87e3402
Shraddha-Shuk <shraddhashuk28@gmail.com>
2024-09-22 11:11:17 -0700

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,44 @@
+import Photo from "@/components/Photo";
+import Socials from "@/components/Socials";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button";
+import { FaDownload } from "react-icons/fa";
+import { FiDownload} from "react-icons/fi";
+
+
+const Home = () => {
+  return (
+    <section className="h-full">
+        <div className="container mx-auto h-full">
+            <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+                {/*text*/}
+                <div className="text-center xl:text-left order-2 xl:order-none">
+                    <span className="text-xl">Full Stack Developer</span>
+                    <h1 className="h1 mb-6">Hello I'm <br /> <span className="text-accent">Shraddha Shukla</span></h1>
+                    <p className="max-w-[500px] mb-9 text-white/80">I excel in making websites and I am proficient in various technologies and also learning LLM.</p>
+                    {/*btn and socials */}
+                    <div className="flex flex-col xl:flex-row items-center gap-8">
+                        <Button 
+                            size = "lg"
+                            className="uppercase gap-2 w-30 h-11 border border-accent justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500 rounded-3xl bg-primary"
+                        >
+                            <span className="">Download CV</span>
+                            < FiDownload className="text-xl"/>
+                        </Button>
+                        <div className="mb-8 xl:mb-0" >
+                            <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+                        </div>
+                    </div>
+                </div>
+                {/*photo*/}
+                <div className="order-1 xl:order-none mb-8 xl:mb-0">
+                    <Photo />
+                </div>
+            </div>
+        </div>
+        <Stats />
+    </section>
+  )
+}
+
+export default Home;
---
+++ app/resume/page.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  FaHtml5, 
+  FaCss3, 
+  FaReact, 
+  FaNodeJs, 
+} from 'react-icons/fa'
+
+import {
+  SiTailwindcss, SiNextjs 
+} from 'react-icons/fa'
+
+const about = {
+  title: 'About me',
+  description: " Final-year B.Tech IT student with expertise in MERN stack development. Experienced in building and managing projects, with a strong foundation in web development and machine learning. Actively expanding skills in large language models (LLM).",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Shraddha Shukla"
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+91) 7732930137"
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "Fresher"
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "shraddhashuk28@gmail.com"
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Indian"
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available"
+    },
+    {
+      fieldName: "Language",
+      fieldValue: "English, Hindi"
+    }
+  ]
+};
+
+//
+
+const Resume = () => {
+  return (
+    <div>RESUME</div>
+  )
+}
+
+export default Resume
